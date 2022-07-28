facb2ab6bf742cb23bb3bdf94c6142fa77877868
preeti28dec <preetithakur28dec2000@gmail.com>
2022-07-28 19:14:24 +0530

    date 28-07-2022 app done

--- pages/componets/header.jsx
+++ pages/componets/header.jsx
@@ -6,9 +6,9 @@ import { Links } from './hamburger'
 
 function Header() {
     return (
-        <section className='container mx-auto pt-5'>
-            <div className='flex justify-between item-center items-center'>
-                <div className='w-[183px] h-[68px] rounded-lg px-4 py-2 flex gap-3 items-center'>
+        <section className='container mx-auto pt-5 main_header'>
+            <div className='flex justify-between item-center items-center main_header1'>
+                <div className=' rounded-lg px-4 py-2 flex gap-3 items-center'>
                     <div className='flex'>
                         <div className='mt-2 mx-2'><img src='./images/logoo.svg' width={40} alt='logo-error' /></div>
                         <div>
@@ -17,10 +17,10 @@ function Header() {
                         </div>
                     </div>
                 </div>
-                <div className=' links flex gap-10 pt-4 font-[400px] text-[18px] font-Quicksand'>
+                <div className=' links flex gap-10 pt-4 font-[400px] text-[18px] font-Quicksand '>
                     <Links />
                 </div>
-                <div className='flex justify-center mt-6'>
+                <div className='flex justify-center mt-6 '>
                     <Link href="mailto:main@gmail.com"><a ><button className='border border-[#377DFF] sm:px-6 text-[14px] px-3  py-2 rounded-full mx-6'>Contact us</button></a></Link>
                     <Test />
                 </div>
