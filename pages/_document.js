71c64849003395645b7ae7b6039b2dbe81850373
preetidec28 <preetithakur28dec2000@gmail.com>
2024-06-09 14:42:07 +0530

    updated code

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json" />
+          {/* <link rel="manifest" href="/manifest.json" /> */}
           <link rel="apple-touch-icon" href="/icon.png" />
           <link href="https://fonts.googleapis.com/css2?family=Inter&display=optional" rel="stylesheet" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
